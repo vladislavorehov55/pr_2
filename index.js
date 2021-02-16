c881d89c358e7e33e2943b453a55e866ebf959f7
vladislavorehov55 <vladislavorehov55@gmail.com>
2021-02-16 04:05:46 +0300

    Task3

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-let money = prompt('Ваш бюджет на месяц?');
+let money = +prompt('Ваш бюджет на месяц?');
 let time = prompt('Введите дату в формате YYYY-MM-DD');
 
-const a = prompt('Введите обязательную статью расходов в этом месяце');
-const cost = prompt('Во сколько обойдется?')
+
 const appData = {
     budget: money, 
     timeData: time,
-    expenses: {[a]: cost},
+    expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false}
+    savings: false
+}
 
-console.log(appData);
+for (let i = 0; i < 1; i++) {
+    const a = prompt('Введите обязательную статью расходов в этом месяце');
+    const b = prompt('Во сколько обойдется?');
+    if (typeof a !== null && typeof b !== null && a !== '' && b !== '') {
+        appData.expenses[a] = b;
+    } else {
+        i--
+    }
+    
+}
+appData.moneyPerDay = appData.budget / 30;
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000) {
+    console.log('Среднмй уровень');
+} else if (appData.moneyPerDay > 1000) {
+    console.log('Высокий урвоень');
+} else {
+    console.log('произошла ошибка');
+}
