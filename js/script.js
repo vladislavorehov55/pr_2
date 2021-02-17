41b405dfcffc2017bfee920d30d23a9d34abb810
vladislavorehov55 <vladislavorehov55@gmail.com>
2021-02-17 22:34:18 +0300

    Task7

--- js/script.js
+++
@@ -1,5 +0,0 @@
-document.body.style.background = 'url(../img/apple_true.jpg) center no-repeat';
-document.getElementById('title').textContent = 'Мы продаем только подлинную технику Apple';
-document.querySelector('.adv').remove();
-const answer = prompt('Каково ваше отношение к технике apple?');
-document.getElementById('prompt').textContent  = answer;
