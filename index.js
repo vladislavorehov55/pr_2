30af1e0f61e855323adfd2d16d27987af3944cf3
vladislavorehov55 <vladislavorehov55@gmail.com>
2021-02-18 22:23:41 +0300

    task13

--- index.js
+++ index.js
@@ -67,4 +67,56 @@ document.addEventListener('DOMContentLoaded', function() {
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     })
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+    let form = document.querySelector('.main-form');
+    let input = form.getElementsByTagName('input');
+    let statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        form.append(statusMessage);
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        let formData = new FormData(form);
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj)
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            }
+            else if (request.readyState === 4 && request.status === 200) {
+                statusMessage.innerHTML = message.success;
+            }
+            else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    })
+
+    const form2 = document.getElementById('form');
+    form2.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const obj ={};
+        const inputs = form2.querySelectorAll('input');
+        inputs.forEach(function(item) {
+            obj[item.name] = item.value;
+        });
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php')
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(obj))
+    })
 })
